01266a5c0d165154a7923b40877788f3638c6b92
sahu-dev-hub <nathpc22@gmail.com>
2026-06-06 19:11:40 +0530

    Add files via upload

---
+++ ubuntu-lts-v5.sh
@@ -0,0 +1,11 @@
+# Ubuntu 24.04 LTS (Noble) - proot-distro v5 installer config
+# proot-distro v5 mein plugin .sh files kaam nahi karti,
+# isliye yahan sirf install command likha hai reference ke liye.
+# Actual install install.sh se hoti hai automatically.
+
+# v5 install command:
+# proot-distro install ubuntu:24.04 --name ubuntu-lts
+
+DISTRO_IMAGE="ubuntu:24.04"
+DISTRO_NAME="ubuntu-lts"
+DISTRO_COMMENT="Ubuntu 24.04 LTS Noble - proot-distro v5"
